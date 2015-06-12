ea3cc76a6ff8e71807df289419b9c34bd9909509
Reiner Jung <rju@informatik.uni-kiel.de>
2015-06-12 17:15:00 +0200

    grammar update

--- org.iobserve.rac.constraint/src/org/iobserve/rac/constraint/ConstraintLangRuntimeModule.java
+++ org.iobserve.rac.constraint/src/org/iobserve/rac/constraint/ConstraintLangRuntimeModule.java
@@ -7,5 +7,5 @@ package org.iobserve.rac.constraint;
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class ConstraintLangRuntimeModule extends org.iobserve.rac.constraint.AbstractConstraintLangRuntimeModule {
-
+	
 }
